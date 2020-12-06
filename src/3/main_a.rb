7e3fff82572ba0e091ca60a966a3e2193646fb3f
Patrick Read <patrick@noyotechnologies.com>
2020-12-06 14:25:14 -0500

    day 3 puzzle a

--- src/3/main_a.rb
+++ src/3/main_a.rb
@@ -4,12 +4,30 @@ DAY_NUM = 3
 RIGHT = 3
 DOWN = 1
 
+def is_tree?(character)
+  character == '#'
+end
+
 def traverse_slope(hill_lines)
-  0
+  trees = 0
+  current_x = 0
+  current_y = 0
+
+  while hill_lines.length > current_y
+    current_line = hill_lines[current_y]
+    line_length = current_line.length
+    tree_found = is_tree?(current_line[current_x % line_length])
+    trees += tree_found ? 1 : 0
+
+    current_x += RIGHT
+    current_y += DOWN
+  end
+
+  trees
 end
 
 def run
   input = get_input(DAY_NUM)
   num_trees = traverse_slope(input)
-  puts "Tree encountered: #{num_trees}"
+  puts "Trees encountered: #{num_trees}"
 end
