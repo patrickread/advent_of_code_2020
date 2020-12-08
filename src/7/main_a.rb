2f313bf2e949a67b5ca58f50c610542fb1a739ff
Patrick Read <patrick@noyotechnologies.com>
2020-12-07 20:57:28 -0500

    rename

--- src/7/main_a.rb
+++ src/7/main_a.rb
@@ -26,12 +26,40 @@ def parse_rule(rule)
 end
 
 def parse_rules(rules)
-  results = rules.map do |rule|
+  rules.map do |rule|
     parse_rule(rule)
   end
 end
 
+def find_parent_color_instances(child_color, rules)
+  rules.select do |rule|
+    rule[:child_colors].any?{ |child| child[:color] == child_color }
+  end
+end
+
+def find_parent_bags(target_color, rules)
+  parents_of_target = []
+  parent_color_instances = find_parent_color_instances(target_color, rules)
+  parents_of_target.concat(parent_color_instances)
+
+  parents = parent_color_instances.map do |parent_color_instance|
+    # Don't need to search ones that are already found
+    find_parent_bags(parent_color_instance[:parent_color], rules - parent_color_instances)
+  end
+
+  parents_of_target.concat(parents.flatten)
+  parents_of_target
+end
+
+def find_unique_parents(target_color, rules)
+  all_parents = find_parent_bags(target_color, rules)
+  all_parents.uniq!{ |x| x[:parent_color]}
+  puts "#{all_parents.length} parents of #{target_color} found!"
+end
+
 def run
   all_rules = get_input(DAY_NUM)
-  parse_rules(all_rules)
+  parsed_rules = parse_rules(all_rules)
+  target_color = 'shiny gold'
+  find_unique_parents(target_color, parsed_rules)
 end
